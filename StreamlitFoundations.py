d99cff8d88b83feb127c325147e37a1d11b8204d
IdoBalter1 <idobalter@gmail.com>
2024-09-24 19:10:18 +0000

    Update StreamlitFoundations.py

--- StreamlitFoundations.py
+++ StreamlitFoundations.py
@@ -20,6 +20,7 @@ def app():
     rev = st.session_state.get("rev", "")
     job_number = st.session_state.get("job_number", 0)
     date = st.session_state.get("date", "")
+    image = st.session_state.get("image", "")
     material = 'foundation'
     def get_inputs():
         title_of_work = st.text_input("Enter the title of the work", value="Foundations Calculation")
@@ -65,7 +66,7 @@ def app():
             """
             # Scale the image to width 50 mm and height 30 mm
             page_number = self.page_no()
-            #self.image("z:\Pdf's Adverts and Logo\logo.jpg", 10, 10, 50)
+            self.image(image, 10, 10, 50)
             self.set_y(25)
             self.set_font('Times', '', 11)
             num_cells = 2
@@ -340,14 +341,17 @@ def app():
     else:
         None
         
+    pdf_file_path = f'{job_number}_{job_title}_Sheet_{sheet_number}.pdf'
+    pdf.output(pdf_file_path)
     finish = st.button("Finish")
     if finish:
-        pdf.output(f'{job_number} {job_title} Sheet{sheet_number} ')
-        pdf_file_path = os.path.abspath(f'{job_number} {job_title} Sheet{sheet_number} ')
-        chrome_command = ["start", "chrome", pdf_file_path]
-        subprocess.run(chrome_command, shell=True)
-    else:
-        None
+       with open(pdf_file_path, "rb") as pdf_file:
+        st.download_button(
+            label="Download PDF",
+            data=pdf_file,
+            file_name=os.path.basename(pdf_file_path),
+            mime="application/pdf"
+        )
 
     def reset_inputs():
         st.session_state.distributed_loads = []
@@ -360,4 +364,5 @@ def app():
         st.success("Inputs have been reset 😊.")
     else:
         None
+        
         
